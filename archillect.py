d277be8ea52cc17421854583ffb6994974498723
Yurii Liulko <yurik.lyulko@gmail.com>
2024-01-22 13:52:01 +0200

    implemented telegram bot

--- archillect.py
+++ archillect.py
@@ -2,33 +2,29 @@ import requests as r
 import bs4
 
 
-def get_max_index() -> int:
+def get_last_post_index() -> int:
     url = 'https://archillect.com/archive'
     html = r.get(url).text
     soup = bs4.BeautifulSoup(html, 'html.parser')
     archive = soup.find(id='archive')
     first_a = archive.find_all('a')[0]
-    post_id = first_a.find(class_="postid").text
+    post_id = int(first_a.find(class_="postid").text)
     return post_id
 
 
 def get_image(post_id: int) -> dict:
     base_url = 'https://archillect.com/'
-    post_page_html = r.get(f'{base_url}{post_id}').text
+    post_url = base_url + str(post_id)
+    post_page_html = r.get(post_url).text
     soup = bs4.BeautifulSoup(post_page_html, 'html.parser')
     ii = soup.find(id='ii')
     image_url = ii['src']
     image_name = image_url.split('/')[-1]
     image_bytes = r.get(ii['src']).content
-
     return {
-        'bytes': image_bytes,
-        'name': image_name
+        'post_id': post_id,
+        'post_url': post_url,
+        'url': image_url,
+        'name': image_name,
+        'bytes': image_bytes
     }
-
-
-last_id = get_max_index()
-image = get_image(last_id)
-with open(f'{image["name"]}', 'wb') as f:
-    f.write(image['bytes'])
-    f.close()
--- telegram.py
+++ telegram.py
@@ -1,2 +1,49 @@
-import telebot
+import random
 
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import creds
+import archillect
+
+bot = telebot.TeleBot(creds.token)
+
+
+def inline_markup():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 2
+    markup.add(InlineKeyboardButton("Get last", callback_data="cb_last"),
+               InlineKeyboardButton("New random", callback_data="cb_random"))
+    return markup
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.send_message(message.chat.id, 'Which photo you want?', reply_markup=inline_markup())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'cb_last')
+def callback_query(call):
+    bot.answer_callback_query(call.id, "receiving last post")
+    post_id = archillect.get_last_post_index()
+    ii = archillect.get_image(post_id)
+    answer = bot.send_photo(call.message.chat.id,
+                            ii['url'],
+                            caption=f'[{ii["post_id"]}]({ii["post_url"]})',
+                            parse_mode='MarkdownV2',
+                            reply_markup=inline_markup())
+
+
+@bot.callback_query_handler(func=lambda call: call.data == 'cb_random')
+def callback_query(call):
+    bot.answer_callback_query(call.id, "receiving random post")
+    post_id = random.randrange(1, archillect.get_last_post_index() + 1)
+    ii = archillect.get_image(post_id)
+    answer = bot.send_photo(call.message.chat.id,
+                            ii['url'],
+                            caption=f'[{ii["post_id"]}]({ii["post_url"]})',
+                            parse_mode='MarkdownV2',
+                            reply_markup=inline_markup())
+
+
+bot.infinity_polling()
