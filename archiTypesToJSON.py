a4c98f150f7cd684a65b43d45d211d71be9448d4
Yurii Liulko <yurik.lyulko@gmail.com>
2024-01-30 17:42:08 +0200

    removed 3 fields for simplify json output

--- archiTypesToJSON.py
+++ archiTypesToJSON.py
@@ -12,10 +12,14 @@ def make_archi_types_json(max_id):
         os.system('clear') if os.name == 'posix' else os.system('cls')
         print(f'work on {i}/{max_id}')
         ii = archillect.get_image(i)
-        to_be_json.append(ii)
+        to_be_json.append({
+            'id': ii['post_id'],
+            'url': ii['url']
+        })
 
     with open(f'out__{datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")}.json', 'w') as f:
         json.dump(to_be_json, f)
 
 
-make_archi_types_json(archillect.get_last_post_index())
+# make_archi_types_json(archillect.get_last_post_index())
+make_archi_types_json(10)
