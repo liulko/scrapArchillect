87dce5ccb7a7828e093b7bf23d7a52e487695ab5
Yurii Liulko <yurik.lyulko@gmail.com>
2024-01-31 14:55:19 +0200

    removed clear console

--- archiTypesToJSON.py
+++ archiTypesToJSON.py
@@ -9,7 +9,7 @@ import json
 def make_archi_types_json(max_id):
     to_be_json = list()
     for i in range(1, max_id + 1):
-        os.system('clear') if os.name == 'posix' else os.system('cls')
+        # os.system('clear') if os.name == 'posix' else os.system('cls')
         print(f'work on {i}/{max_id}')
         ii = archillect.get_image(i)
         to_be_json.append({
