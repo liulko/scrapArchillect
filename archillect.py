5191c21109ba5b27407a09529daadb92ca16d22e
Yurii Liulko <yurik.lyulko@gmail.com>
2024-01-31 14:50:34 +0200

    fixed typo

--- archillect.py
+++ archillect.py
@@ -1,6 +1,6 @@
 import requests as r
 import bs4
-
+import creds
 
 def get_last_post_index() -> int:
     url = 'https://archillect.com/archive'
@@ -18,13 +18,13 @@ def get_image(post_id: int) -> dict:
     response = r.get(post_url)
     print(f'{post_id}: {response.status_code}')
     while not response.status_code == 200:
-        proxy = r.get('http://51.20.244.123:5000/api/proxy').text
+        proxy = r.get(creds.proxy_api_address).text
         proxies = {
             'http': proxy,
             'https': proxy
         }
         response = r.get(post_url, proxies=proxies)
-        print(f'{post_id}: {response.status_code} with {proxy.split(':')[-2]}')
+        print(f'{post_id}: {response.status_code} with {proxy.split(":")[-2]}')
 
     post_page_html = response.text
     soup = bs4.BeautifulSoup(post_page_html, 'html.parser')
