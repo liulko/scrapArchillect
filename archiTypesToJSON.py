c9e414853502d9fbdbdcf65824d60baca2fb80e1
Yurii Liulko <yurik.lyulko@gmail.com>
2024-02-05 07:12:46 +0200

    fixed output file name to correct last index. changed id for processing

--- archiTypesToJSON.py
+++ archiTypesToJSON.py
@@ -19,7 +19,7 @@ def make_archi_types_json(start_id, end_id):
             'url': ii['url']
         })
         if counter % 1000 == 0:
-            with open(f'out__{start_id}_{start_id + counter}__{datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")}.json',
+            with open(f'out__{start_id}_{start_id + counter - 1}__{datetime.datetime.now().strftime("%Y_%m_%d__%H_%M_%S")}.json',
                       'w') as f:
                 json.dump(to_be_json, f)
 
@@ -28,4 +28,4 @@ def make_archi_types_json(start_id, end_id):
 
 
 # make_archi_types_json(archillect.get_last_post_index())
-make_archi_types_json(1001, 5000)
+make_archi_types_json(94000, 400000)
