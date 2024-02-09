1f279e43fb57fb3b926c4a4fe7d63ae4541f482c
Yurii Liulko <yurik.lyulko@gmail.com>
2024-02-09 12:52:56 +0200

    making 2 different lists for photos and gifs.

--- archillect_local.py
+++ archillect_local.py
@@ -5,8 +5,13 @@ import requests as r
 import bs4
 import creds
 
-ARCHI_ARCHIVE = [i for i in json.loads(open('archillect_final_dict.json', 'rb').read())]
-print(len(ARCHI_ARCHIVE))
+ARCHI_ARCHIVE = [i for i in json.loads(open('archillect_final_dict.json', 'rb').read()) if str(i['url']) != 'False']
+ARCHI_GIF = [i for i in ARCHI_ARCHIVE if '.gif' in str(i['url'])[-4:]]
+ARCHI_PHOTO = [i for i in ARCHI_ARCHIVE if ('.jpg' in str(i['url'])[-4:]) or ('.png' in str(i['url'])[-4:])]
+
+print(f'{len(ARCHI_ARCHIVE)} total')
+print(f'{len(ARCHI_GIF)} gif')
+print(f'{len(ARCHI_PHOTO)} png or jpg')
 
 
 def get_last_post_index() -> int:
@@ -25,3 +30,7 @@ def get_image(post_id: int) -> dict:
         'name': image_name,
         'image_type': image_type
     }
+
+
+def get_animation():
+    return
